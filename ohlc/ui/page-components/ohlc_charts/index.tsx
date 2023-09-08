0223e9ae36a37194f4dfbb0ffa78d7a782b8a6c5
Vikash Patidar <vpatidar@deqode.com>
2023-09-08 14:35:58 +0530

    dynamic import

--- ohlc/ui/page-components/ohlc_charts/index.tsx
+++ ohlc/ui/page-components/ohlc_charts/index.tsx
@@ -4,18 +4,21 @@ import { QueryClientProvider, QueryClient } from "react-query";
 const queryClient = new QueryClient();
 import Book from "./components/Book";
 import { useState } from "react";
-// import TradingView from "../trading-view/components/page";
 import Header from "./components/Header/page";
 import LightChart from './components/LightChart/page'
+import dynamic from 'next/dynamic';
+const TradingView = dynamic(() => import('../trading-view/components/page'), {
+  ssr: false,
+});
 
 const OHLC = () => {
   const [selectType, setSelcetType] = useState('Chart')
 
- 
+
   const renderMapping = {
-    'Chart':<LightChart/>,
-    'Book': <Book  />,
-    // "TradingView": <TradingView />
+    'Chart': <LightChart />,
+    'Book': <Book />,
+    "TradingView": <TradingView />
   }
   const Component = renderMapping[selectType] || null
 
@@ -23,7 +26,7 @@ const OHLC = () => {
     <div>
       <QueryClientProvider client={queryClient}>
         <Header setSelcetType={setSelcetType} selectType={selectType} />
-        
+
         {Component ? Component : null}
       </QueryClientProvider>
     </div>
